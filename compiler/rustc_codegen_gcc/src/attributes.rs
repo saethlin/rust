aa82970673a3e7975471ef71577ef1189785d9ea
Ben Kimock <kimockb@gmail.com>
2024-07-26 18:18:19 -0400

    Fix gcc

--- compiler/rustc_codegen_gcc/src/attributes.rs
+++ compiler/rustc_codegen_gcc/src/attributes.rs
@@ -47,7 +47,7 @@ pub fn from_fn_attrs<'gcc, 'tcx>(
         let inline = if codegen_fn_attrs.flags.contains(CodegenFnAttrFlags::NAKED) {
             InlineAttr::Never
         } else if codegen_fn_attrs.inline == InlineAttr::None
-            && tcx.cross_crate_inlinable(instance.def_id())
+            && cx.tcx.cross_crate_inlinable(instance.def_id())
         {
             InlineAttr::Hint
         } else {
