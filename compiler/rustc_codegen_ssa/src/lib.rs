6f5792ceb2e030f1504a88e0d67fb0b10e5ae495
Ben Kimock <kimockb@gmail.com>
2024-07-28 16:54:14 -0400

    Avoid no-op unlink+link dances in incr comp

--- compiler/rustc_codegen_ssa/src/lib.rs
+++ compiler/rustc_codegen_ssa/src/lib.rs
@@ -94,6 +94,7 @@ impl<M> ModuleCodegen<M> {
             bytecode,
             assembly,
             llvm_ir,
+            links_from_incr_cache: Vec::new(),
         }
     }
 }
@@ -107,6 +108,7 @@ pub struct CompiledModule {
     pub bytecode: Option<PathBuf>,
     pub assembly: Option<PathBuf>, // --emit=asm
     pub llvm_ir: Option<PathBuf>,  // --emit=llvm-ir, llvm-bc is in bytecode
+    pub links_from_incr_cache: Vec<PathBuf>,
 }
 
 impl CompiledModule {
