3684d8858adb558235ee54dd601d6384afea83c5
Ben Kimock <kimockb@gmail.com>
2024-09-27 22:10:03 -0400

    Cache for local queries

--- compiler/rustc_middle/src/query/mod.rs
+++ compiler/rustc_middle/src/query/mod.rs
@@ -1498,6 +1498,7 @@ rustc_queries! {
     }
     query mir_flags(key: DefId) -> MirFlags {
         desc { |tcx| "stashing some local properties of `{}` before the body is stolen", tcx.def_path_str(key) }
+        cache_on_disk_if { key.is_local() }
         separate_provide_extern
     }
     query required_panic_strategy(_: CrateNum) -> Option<PanicStrategy> {
