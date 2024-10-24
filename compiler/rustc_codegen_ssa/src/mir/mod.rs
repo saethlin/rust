4ae354213c71cb0032f93534f88c49b69c704aaf
Ben Kimock <kimockb@gmail.com>
2024-10-23 22:44:53 -0400

    Use worse MIR for incr comp

--- compiler/rustc_codegen_ssa/src/mir/mod.rs
+++ compiler/rustc_codegen_ssa/src/mir/mod.rs
@@ -159,7 +159,7 @@ impl<'tcx, V: CodegenObject> LocalRef<'tcx, V> {
 ///////////////////////////////////////////////////////////////////////////
 
 #[instrument(level = "debug", skip(cx))]
-pub fn codegen_mir<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>>(
+pub fn lower_mir<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>>(
     cx: &'a Bx::CodegenCx,
     instance: Instance<'tcx>,
 ) {
